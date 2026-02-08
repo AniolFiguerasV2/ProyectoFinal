7a0410a9d99100533573efdd0a29f212b2b8ee67
Marctupadre <116338801+Marctupadre@users.noreply.github.com>
2026-02-08 13:28:31 +0100

    finishing control of ambulance with testcar
    aaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- Assets/Carpeta Marc/Scripts/AmbulanceController.cs
+++ Assets/Carpeta Marc/Scripts/AmbulanceController.cs
@@ -1,7 +1,75 @@
+using Unity.VisualScripting;
+using Unity.VisualScripting.InputSystem;
 using UnityEngine;
 
 public class AmbulanceController : MonoBehaviour
 {
+    [Header("Ambulance Properties")]
+    public float motorTorque = 2000f;
+    public float brakeTorque = 2000f;
+    public float maxSpeed = 20f;
+    public float steerinRange = 30f;
+    public float steeringRangeAtMaxSpeed = 10f;
+    public float centreOfGravityOffset = -1f;
+
+    private WheelControl[] wheels;
+    private Rigidbody rb;
+
+    private PlayerController carControls;
+
+    void Awake()
+    {
+        carControls = new PlayerController();
+    }
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Vector3 centerOfMass = rb.centerOfMass;
+        centerOfMass.y += centreOfGravityOffset;
+        rb.centerOfMass = centerOfMass;
+
+        wheels = GetComponentsInChildren<WheelControl>();
+    }
+
+    void FixedUpdate()
+    {
+        //Vector2 inputVector = carControls.
+
+        //float vInput = inputVector.y;
+        //float hInput = inputVector.x;
+
+        float forwardSpeed = Vector3.Dot(transform.forward, rb.linearVelocity);
+        float speedFactor =Mathf.InverseLerp(0, maxSpeed, Mathf.Abs(forwardSpeed));
+
+        float currentMotorTorque = Mathf.Lerp(motorTorque, 0, speedFactor);
+        float currentSteerRange = Mathf.Lerp(steerinRange, steeringRangeAtMaxSpeed, speedFactor);
+
+        //bool isAccelerating = Mathf.Sign(vInput) == Mathf.Sign(forwardSpeed);
+
+        foreach (var wheel in wheels)
+        {
+            if(wheel.steerable)
+            {
+                //wheel.wheelCollider.steerAngle = hInput * currentSteerRange;
+            }
+            /*
+            if(isAccelerating)
+            {
+                if(wheel.motorized)
+                {
+                    wheel.WheelCollider.notorTorque = vInput * currentMotorTorque;
+                }
+                wheel.WheelCollider.brakeTorque = 0f;
+            }
+            else
+            {
+                wheel.WheelCollider.motorTorque = 0f;
+                wheel.WheelCollider.brakeTorque = Mathf.Abs(vInput) * brakeTorque;
+            }
+            */
+        }
+    }
     public void EnterVehicle(InteractPlayers player)
     {
         player.transform.SetParent(transform);
@@ -18,5 +86,16 @@ public class AmbulanceController : MonoBehaviour
         player.WalkMode();
         //player.playervisual.SetActive(true);
         //player.movementscript.enabled = true;
+    }
+
+
+    void OnEnable()
+    {
+        carControls.enabled = true;
+    }
+
+    void OnDisable()
+    {
+        carControls.enabled = false;
     }
 }
