15c27ffcd2c8ad9f308495477322bba28d3172f0
Marctupadre <116338801+Marctupadre@users.noreply.github.com>
2026-02-10 17:59:21 +0100

    cambios camilla
    Co-Authored-By: AniolFiguerasV2 <235719381+AniolFiguerasV2@users.noreply.github.com>

--- Assets/Scripts/Handles.cs
+++ Assets/Scripts/Handles.cs
@@ -1,10 +1,32 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class Handles : MonoBehaviour
 {
+    public Transform player;
+    public Transform snapPoint;
 
+    private int requirePlayers = 2;
+    private int curerntPlayers = 0;
+
+    private bool isPlayerGrabing = false;
+    private PlayerActions playerActions;
+
+
+    private void Awake()
+    {
+        playerActions = new PlayerActions();
+    }
+    private void Update()
+    {
+        playerActions.Player1.Grab.ReadValue<Button>();
+    }
     private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.gameObject.CompareTag("Player1") || other.gameObject.CompareTag("Player2"))
+        {
+
+        }
     }
 }
