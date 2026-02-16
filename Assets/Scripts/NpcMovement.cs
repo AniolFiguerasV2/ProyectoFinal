02611664e5652f566693e959d6125731a401e904
Marctupadre <116338801+Marctupadre@users.noreply.github.com>
2026-02-16 18:54:33 +0100

    Waypoints basic movement
    aaaaaaaaaaa

---
+++ Assets/Scripts/NpcMovement.cs
@@ -0,0 +1,27 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcMovement : MonoBehaviour
+{
+    public List<GameObject> wayPoints;
+    public float speed = 2;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        int index = 0;
+        Vector3 destination = wayPoints[index].transform.position;
+        Vector3 newPos = Vector3.MoveTowards(transform.position, wayPoints[index].transform.position, speed * Time.deltaTime);
+        transform.position = newPos;
+
+        float distance = Vector3.Distance(transform.position, destination);
+        if (distance <= 0.05f)
+        {
+            index++;
+        }
+    }   
+}
