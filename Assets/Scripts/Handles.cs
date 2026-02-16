6183ccc479b2a86b09dabe5992b68eba4b5141eb
AniolFiguerasV2 <aniol.figueras@estudiant.enti.cat>
2026-02-16 13:37:09 +0100

    Intento de hacer las handles

--- Assets/Scripts/Handles.cs
+++ Assets/Scripts/Handles.cs
@@ -4,29 +4,115 @@ using UnityEngine.UI;
 
 public class Handles : MonoBehaviour
 {
-    public Transform player;
     public Transform snapPoint;
 
-    private int requirePlayers = 2;
-    private int curerntPlayers = 0;
+    private PlayerActions player1;
+    private PlayerActions player2;
 
-    private bool isPlayerGrabing = false;
-    private PlayerActions playerActions;
+    private Transform player1Transform;
+    private Transform player2Transform;
 
+    private bool player1InZone;
+    private bool player2InZone;
+
+    private int holder = 0;
 
     private void Awake()
     {
-        playerActions = new PlayerActions();
+        player1 = new PlayerActions();
+        player2 = new PlayerActions();
+        player1.Enable();
+        player2.Enable();
     }
-    private void Update()
-    {
-        playerActions.Player1.Grab.ReadValue<Button>();
-    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Player1") || other.gameObject.CompareTag("Player2"))
+        if (other.CompareTag("Player1"))
         {
-
+            player1InZone = true;
+            player1Transform = other.transform;
         }
+        if (other.CompareTag("Player2"))
+        {
+            player2InZone = true;
+            player2Transform = other.transform;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player1"))
+        {
+            player1InZone = false;
+            if (holder == 1)
+            {
+                Release();
+            }
+        }
+
+        if (other.CompareTag("Player2"))
+        {
+            player2InZone = false;
+            if (holder == 2)
+            {
+                Release();
+            }
+        }
+    }
+
+    private void Update()
+    {
+        float grab1 = player1.Player1.Grab.ReadValue<float>();
+        float grab2 = player2.Player2.Grab.ReadValue<float>();
+
+
+        if (holder == 1)
+        {
+            player1Transform.position = snapPoint.position;
+            player1Transform.rotation = snapPoint.rotation;
+        }
+        else if (holder == 2)
+        {
+            player2Transform.position = snapPoint.position;
+            player2Transform.rotation = snapPoint.rotation;
+        }
+
+        if (holder == 0)
+        {
+            if (player1InZone && grab1 > 0.5f)
+            {
+                Grab(1);
+            }
+            else if (player2InZone && grab2 > 0.5f)
+            {
+                Grab(2);
+            }
+        }
+        else
+        {
+            if (holder == 1 && grab1 < 0.5f)
+            {
+                Release();
+            }
+            if (holder == 2 && grab2 < 0.5f)
+            {
+                Release();
+            }   
+        }
+    }
+
+    private void Grab(int player)
+    {
+        holder = player;
+
+        Transform snap = (player == 1) ? player1Transform : player2Transform;
+
+        snap.position = snapPoint.position;
+        snap.rotation = snapPoint.rotation;
+    }
+
+    private void Release()
+    {
+            holder = 0;
     }
 }
