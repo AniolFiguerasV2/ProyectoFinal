2d14e4bd571604de05681fea696713c7a3a82da4
Marctupadre <116338801+Marctupadre@users.noreply.github.com>
2026-02-26 23:37:54 +0100

    prototipo hecho
    aaaaaaaaaaaaa

---
+++ Assets/Scripts/Canvas/Exitgame.cs
@@ -0,0 +1,11 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Exitgame : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/IA/PutPacientStrecher.cs
@@ -0,0 +1,43 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+
+public class PutPacientStrecher : MonoBehaviour
+{
+    public Transform refcamilla;
+    public Transform salaespera;
+
+    public GameObject panelwin;
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Strecher"))
+        {
+            Vector3 offset = new Vector3(0f, 0.5f, 0f);
+            gameObject.transform.position = refcamilla.position + offset;
+            gameObject.transform.SetParent(refcamilla);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Hospital"))
+        {
+            panelwin.gameObject.SetActive(true);
+            Destroy(gameObject);
+        }
+    }
+}
