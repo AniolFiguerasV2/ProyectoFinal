402ad031fd1045153c666d214632ae09044440a0
Gerard Claramunt <gerard.claramunt@enti.cat>
2026-03-21 19:01:43 +0100

    UI avanzada

--- Assets/Scripts/UI/StartTutorialManager.cs
+++ Assets/Scripts/UI/StartTutorialManager.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StartTutorialManager : MonoBehaviour
 {
     public GameObject tutorialPanel1;
     public GameObject tutorialPanel2;
+
+    public GameObject[] panels;
 
     private void Start()
     {
@@ -24,5 +27,34 @@ public class StartTutorialManager : MonoBehaviour
         tutorialPanel1.SetActive(false);
         tutorialPanel2.SetActive(false);
         Time.timeScale = 1f;
+    }
+
+
+    public void ShowPanel(GameObject panelToShow)
+    {
+        // Desactivar todos
+        foreach (GameObject panel in panels)
+        {
+            panel.SetActive(false);
+        }
+
+        // Activar el seleccionado
+        panelToShow.SetActive(true);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+
+        // Para que funcione dentro del editor de Unity
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+
+    public void StartGame()
+    {
+        Time.timeScale = 1f; // por si estaba pausado
+        SceneManager.LoadScene("Escena Final1"); 
     }
 }
