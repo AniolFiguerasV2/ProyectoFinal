d2a5c21c0a25b5413b08ff6b24712a9f45a5b0c0
Gerard Claramunt <gerard.claramunt@enti.cat>
2026-04-22 20:30:39 +0200

    Bug ambulance entry arreglado

--- Assets/Reinicio/Scripts/Ambulance/AmbulanceEntry.cs
+++ Assets/Reinicio/Scripts/Ambulance/AmbulanceEntry.cs
@@ -15,9 +15,6 @@ public class AmbulanceEntry : MonoBehaviour
     public GameObject player1UI;
     public GameObject player2UI;
 
-    private bool player1InRange = false;
-    private bool player2InRange = false;
-
     private void Start()
     {
         if (player1UI != null)
@@ -27,57 +24,20 @@ public class AmbulanceEntry : MonoBehaviour
             player2UI.SetActive(false);
     }
 
-    private void Update()
-    {
-        if (player1InRange && InputManager.Instance != null && InputManager.Instance.GetInteractDown(1))
-        {
-            InteractPlayers interactPlayer = player1.GetComponent<InteractPlayers>();
-            if (interactPlayer != null)
-            {
-                interactPlayer.currentEntry = this;
-                ambulance.EnterVehicle(interactPlayer, ControlsStearing);
-            }
-        }
-
-        if (player2InRange && InputManager.Instance != null && InputManager.Instance.GetInteractDown(2))
-        {
-            InteractPlayers interactPlayer = player2.GetComponent<InteractPlayers>();
-            if (interactPlayer != null)
-            {
-                interactPlayer.currentEntry = this;
-                ambulance.EnterVehicle(interactPlayer, ControlsStearing);
-            }
-        }
-    }
-
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Ha entrado en trigger: " + other.name);
-
         if (!Available) return;
 
         if (other.gameObject == player1 || other.transform.IsChildOf(player1.transform))
         {
-            Debug.Log("Player 1 en rango");
-            player1InRange = true;
-
             if (player1UI != null)
-            {
                 player1UI.SetActive(true);
-                Debug.Log("Activando UI Player 1");
-            }
         }
 
         if (other.gameObject == player2 || other.transform.IsChildOf(player2.transform))
         {
-            Debug.Log("Player 2 en rango");
-            player2InRange = true;
-
             if (player2UI != null)
-            {
                 player2UI.SetActive(true);
-                Debug.Log("Activando UI Player 2");
-            }
         }
     }
 
@@ -85,16 +45,12 @@ public class AmbulanceEntry : MonoBehaviour
     {
         if (other.gameObject == player1 || other.transform.IsChildOf(player1.transform))
         {
-            player1InRange = false;
-
             if (player1UI != null)
                 player1UI.SetActive(false);
         }
 
         if (other.gameObject == player2 || other.transform.IsChildOf(player2.transform))
         {
-            player2InRange = false;
-
             if (player2UI != null)
                 player2UI.SetActive(false);
         }
